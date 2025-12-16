fe76a633b64515e2df8df2f096bda7e6c59dcfe4
Tomáš Lapos <65809639+tomaslapos@users.noreply.github.com>
2025-12-16 17:13:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // LiquiMixer Service Worker
 // DŮLEŽITÉ: Změna verze vynutí aktualizaci cache u všech uživatelů
-const CACHE_NAME = 'liquimixer-v37';
+const CACHE_NAME = 'liquimixer-v38';
 
 // Soubory pro precaching
 const urlsToCache = [
