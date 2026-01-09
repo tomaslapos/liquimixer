22a2c069b7b65cc633def2c3647555a2b3503a8d
Tomáš Lapos <tomas.lapos@woos.cz>
2026-01-09 16:38:34 +0100

    fix: Improve auth modal titles and Clerk localization

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // LiquiMixer Service Worker
 // DŮLEŽITÉ: Změna verze vynutí aktualizaci cache u všech uživatelů
-const CACHE_NAME = 'liquimixer-v72';
+const CACHE_NAME = 'liquimixer-v73';
 
 // Soubory pro precaching
 const urlsToCache = [
