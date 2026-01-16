bf8d1bdef85ade5cfb4d13d31b530d78c2fe43e5
Tomáš Lapos <tomas.lapos@woos.cz>
2026-01-16 10:44:16 +0100

    reduce-page-padding-top-to-68px

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // LiquiMixer Service Worker
 // DŮLEŽITÉ: Změna verze vynutí aktualizaci cache u všech uživatelů
-const CACHE_NAME = 'liquimixer-v115';
+const CACHE_NAME = 'liquimixer-v117';
 
 // Soubory pro precaching
 const urlsToCache = [
