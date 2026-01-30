341f6e0aa4fed28bfa2b3de1758b8fcdfaee3b06
Tomáš Lapos <tomas.lapos@woos.cz>
2026-01-30 14:51:08 +0100

    Fix UI: toggle switches, stock bar layout, back button in recipe database detail

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // LiquiMixer Service Worker
 // DŮLEŽITÉ: Změna verze vynutí aktualizaci cache u všech uživatelů
-const CACHE_NAME = 'liquimixer-v132';
+const CACHE_NAME = 'liquimixer-v133';
 
 // Soubory pro precaching
 const urlsToCache = [
