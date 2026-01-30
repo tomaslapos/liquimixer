6d2af7967598f9a2068e26213a3f3ef46e5d5e12
Tomáš Lapos <tomas.lapos@woos.cz>
2026-01-30 15:27:23 +0100

    Fix-database-filters-grid-layout-and-back-button

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // LiquiMixer Service Worker
 // DŮLEŽITÉ: Změna verze vynutí aktualizaci cache u všech uživatelů
-const CACHE_NAME = 'liquimixer-v134';
+const CACHE_NAME = 'liquimixer-v135';
 
 // Soubory pro precaching
 const urlsToCache = [
