36294af3cd1ce1811d55dfb1b58c0923f3c3e90d
Tomáš Lapos <tomas.lapos@woos.cz>
2026-02-14 13:24:59 +0100

    seo-update-contact-prefill-sw219

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // LiquiMixer Service Worker
 // DŮLEŽITÉ: Změna verze vynutí aktualizaci cache u všech uživatelů
-const CACHE_NAME = 'liquimixer-v218';
+const CACHE_NAME = 'liquimixer-v219';
 
 // Soubory pro precaching
 const urlsToCache = [
